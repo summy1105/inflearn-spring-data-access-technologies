e56acdc3924f7bf7a77f96db97b85f939f37eb00
summy1105 <73230224+summy1105@users.noreply.github.com>
2024-07-06 22:34:00 +0900

    transaction, lock

---
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -0,0 +1,63 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 파라미터 연동, 풀을 고려한 종료
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV2 {
+
+    private final DataSource dataSource;
+    private final MemberRepositoryV2 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Connection conn = dataSource.getConnection();
+        try {
+            conn.setAutoCommit(false);
+            // 비즈니스 로직 수정
+            bizLogic(conn, fromId, toId, money);
+
+            conn.commit();
+        } catch (Exception e) {
+            conn.rollback();
+            throw e;
+        } finally {
+            releaseConnection(conn);
+        }
+    }
+
+    private void bizLogic(Connection conn, String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(conn, fromId);
+        Member toMember = memberRepository.findById(conn, toId);
+
+        memberRepository.update(conn, fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(conn, toId, toMember.getMoney() + money);
+    }
+
+    private void releaseConnection(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.setAutoCommit(true);
+                conn.close(); // 풀에 반환
+            } catch (Exception e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("예체중 예외 발생");
+        }
+    }
+}
